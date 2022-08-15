40b2ef6570d0b17cd866e3807b030ce9ba69a11d
blueb <76005739+blueb442@users.noreply.github.com>
2022-08-15 13:37:48 -0400

    Uploaded v1.2.0

---
+++ src/main/java/me/blueb442/hyacinthhello/CommandJoinmsg.java
@@ -0,0 +1,52 @@
+package me.blueb442.hyacinthhello;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.UUID;
+
+import static org.bukkit.Bukkit.getServer;
+
+
+public class CommandJoinmsg implements CommandExecutor {
+
+    // This method is called, when somebody uses our command
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        Player p = (Player)sender;
+        String sentence1 = String.join(" ", args);
+        String sentence2 = sentence1.replaceAll("\\[", " ");
+        String sentence = sentence2.replaceAll("\\]", " ");
+        String pfx =  getServer().getPluginManager().getPlugin("HyacinthHello").getConfig().getString("prefix");
+        p.sendMessage(pfx + " Set your join message to " + sentence);
+        UUID joinerUUID = p.getUniqueId();
+        File userdata = new File(((Plugin) Objects.requireNonNull(getServer().getPluginManager().getPlugin("HyacinthHello"))).getDataFolder(), File.separator + "PlayerDatabase");
+        File f = new File(userdata, File.separator + joinerUUID + ".yml");
+        FileConfiguration playerData = YamlConfiguration.loadConfiguration(f);
+        if (p.hasPermission("hyacinthhello.joinmessage")) {
+            if (sentence.length() <= getServer().getPluginManager().getPlugin("HyacinthHello").getConfig().getInt("maximum-message-length")) {
+                try {
+                    playerData.createSection("message");
+                    playerData.set("message.hello", sentence);
+                    playerData.save(f);
+                } catch (IOException var15) {
+                    var15.printStackTrace();
+                }
+            } else if (sentence.length() > getServer().getPluginManager().getPlugin("HyacinthHello").getConfig().getInt("maximum-message-length")) {
+                p.sendMessage(pfx + " That's too long! Your message has to be at most " + getServer().getPluginManager().getPlugin("HyacinthHello").getConfig().getInt("maximum-message-length") + " characters!");
+            }
+        }
+
+        // If the player (or console) uses our command correct, we can return true
+        return true;
+    }
+}
---
+++ src/main/java/me/blueb442/hyacinthhello/HyacinthHello.java
@@ -0,0 +1,153 @@
+package me.blueb442.hyacinthhello;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.entity.Player;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.UUID;
+
+public class HyacinthHello extends JavaPlugin implements Listener, CommandExecutor {
+    FileConfiguration config = this.getConfig();
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        getServer().getPluginManager().registerEvents(this, this);
+
+        this.getCommand("joinmsg").setExecutor(new CommandJoinmsg());
+        this.getCommand("leavemsg").setExecutor(new CommandLeavemsg());
+
+        // Begin forming default config
+        config.addDefault("enabled", true);
+        config.addDefault("prefix", "§dHyacinthHello §8»§r");
+        config.addDefault("hello-wrapper-left", "§6»§e ");
+        config.addDefault("hello-wrapper-right", " §6«");
+        config.addDefault("maximum-message-length", 60);
+
+        config.options().copyDefaults(true);
+        saveConfig();
+
+        if (config.getBoolean("enabled")) {
+            return;
+        } else {
+            Bukkit.getLogger().info("Disabled by config.yml! Set enabled to true in config.yml to enable HyacinthHello.");
+            getServer().getPluginManager().disablePlugin(this);
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+
+        Bukkit.getLogger().info("Saving data and shutting down. Goodbye!");
+
+    }
+
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player p = event.getPlayer();
+
+        UUID joinerUUID = p.getUniqueId();
+        File userdata = new File(Objects.requireNonNull(Bukkit.getServer().getPluginManager().getPlugin("HyacinthHello")).getDataFolder(), File.separator + "PlayerDatabase");
+        File f = new File(userdata, File.separator + joinerUUID + ".yml");
+        FileConfiguration playerData = YamlConfiguration.loadConfiguration(f);
+
+        //When the player file is created for the first time...
+        if (!f.exists()) {
+            try {
+
+                playerData.createSection("message");
+                playerData.set("message.hello", "");
+                playerData.set("message.goodbye", "");
+
+                playerData.save(f);
+
+                Bukkit.getLogger().info("Created a data file for player " + joinerUUID + "(" + p.getName() + ")");
+            } catch (IOException exception) {
+
+                exception.printStackTrace();
+            }
+        }
+
+        String hwl = this.getConfig().getString("hello-wrapper-left");
+        String hwr = this.getConfig().getString("hello-wrapper-right");
+
+        // haha iconic user-made pun goes here
+        String message = playerData.getString("message.hello");
+
+        if (Objects.equals(playerData.getString("message.hello"), "")) {
+            Bukkit.getLogger().info("No hello found for " + joinerUUID);
+            return;
+        } else if (Objects.equals(playerData.getString("message.hello"), null)) {
+            Bukkit.getLogger().info("No hello found for " + joinerUUID);
+            return;
+        } else {
+            Bukkit.getScheduler().runTaskLater(this, () -> {
+                Bukkit.broadcastMessage(hwl + message + hwr);
+                Bukkit.getLogger().info("Hello sent for " + joinerUUID);
+            },  2L); // delay for # of ticks, so it appears UNDER {user} joined.
+        }
+
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player p = event.getPlayer();
+
+        UUID quitterUUID = p.getUniqueId();
+        File userdata = new File(Objects.requireNonNull(Bukkit.getServer().getPluginManager().getPlugin("HyacinthHello")).getDataFolder(), File.separator + "PlayerDatabase");
+        File f = new File(userdata, File.separator + quitterUUID + ".yml");
+        FileConfiguration playerData = YamlConfiguration.loadConfiguration(f);
+
+        //When the player file is created for the first time...
+        if (!f.exists()) {
+            try {
+
+                playerData.createSection("message");
+                playerData.set("message.hello", "");
+                playerData.set("message.goodbye", "");
+
+                playerData.save(f);
+
+                Bukkit.getLogger().info("Created a data file for player " + quitterUUID + "(" + p.getName() + ")");
+            } catch (IOException exception) {
+
+                exception.printStackTrace();
+            }
+        }
+
+        String hwl = this.getConfig().getString("hello-wrapper-left");
+        String hwr = this.getConfig().getString("hello-wrapper-right");
+
+        // haha iconic user-made pun goes here
+        String message = playerData.getString("message.goodbye");
+
+        if (Objects.equals(playerData.getString("message.goodbye"), "")) {
+            Bukkit.getLogger().info("No goodbye found for " + quitterUUID);
+            return;
+        } else if (Objects.equals(playerData.getString("message.goodbye"), null)) {
+            Bukkit.getLogger().info("No goodbye found for " + quitterUUID);
+            return;
+        } else {
+            Bukkit.getScheduler().runTaskLater(this, () -> {
+                Bukkit.broadcastMessage(hwl + message + hwr);
+                Bukkit.getLogger().info("Goodbye sent for " + quitterUUID);
+            },  2L); // delay for # of ticks, so it appears UNDER {user} left.
+        }
+
+    }
+
+}
